2a5c5818336c1a82dd175d0f1de46ee14943a25c
merchi <merchiang@gmail.com>
2025-06-14 20:39:18 +0800

    Add files via upload

--- 2 statement exercise.sql
+++ 2 statement exercise.sql
@@ -1,4 +1,10 @@
-SELECT * 
-FROM employee_demographics
-INNER JOIN employee_salary
-	ON employee_demographics.employee_id = employee_salary.employee_id;
+SELECT emp1.employee_id as emp_santa,
+emp1.first_name as emp_first_santa, 
+emp1.last_name as emp_last_name,
+emp2.employee_id as emp_name,
+emp2.employee_id as emp_first_emp,
+emp2.employee_id as emp_last_emp
+FROM employee_demographics emp1
+JOIN employee_salary emp2
+	ON emp1.employee_id + 1 = emp2.employee_id
+;
